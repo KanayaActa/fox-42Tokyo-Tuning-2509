3453babf29e7b519492151b13799737a36aeae52
Andrei Enshin <aenshin@c3r4s2.42tokyo.jp>
2025-09-27 00:00:57 +0900

    improve indexes

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -1,3 +1,5 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
-CREATE INDEX idx_customer_lastname ON orders (order_id);
-CREATE INDEX idx_customer_lastname ON orders (shipped_status);
+CREATE INDEX idx1 ON orders (order_id);
+CREATE INDEX idx2 ON orders (shipped_status);
+CREATE INDEX idx3 ON users (user_name);
+CREATE INDEX idx4 ON user_sessions (user_id);
