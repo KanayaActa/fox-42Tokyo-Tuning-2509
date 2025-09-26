7f01e382d73b3329f0b406ebbdfaa0fb7892fb00
Andrei Enshin <aenshin@c3r4s2.42tokyo.jp>
2025-09-27 00:12:33 +0900

    add more idxs

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -1,5 +1,11 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
 CREATE INDEX idx1 ON orders (order_id);
 CREATE INDEX idx2 ON orders (shipped_status);
+
 CREATE INDEX idx3 ON users (user_name);
+
 CREATE INDEX idx4 ON user_sessions (user_id);
+
+CREATE INDEX idx5 ON products (name);
+CREATE INDEX idx6 ON products (description);
+CREATE INDEX idx7 ON products (product_id);
