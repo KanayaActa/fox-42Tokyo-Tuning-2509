2cb3db7362c7377811c9828a21a331ddb3edc8c3
yuo_ota <yuuta.tsunashima@gmail.com>
2025-09-27 19:58:27 +0900

    fix : conflict

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -1,5 +1,6 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
-<<<<<<< HEAD
+update users set password_hash = '5f4dcc3b5aa765d61d8327deb882cf99';
+
 CREATE INDEX idx1 ON orders (order_id);
 CREATE INDEX idx2 ON orders (shipped_status);
 
@@ -10,6 +11,3 @@ CREATE INDEX idx4 ON user_sessions (user_id);
 CREATE INDEX idx5 ON products (name);
 CREATE INDEX idx6 ON products (description);
 CREATE INDEX idx7 ON products (product_id);
-=======
-update users set password_hash = '5f4dcc3b5aa765d61d8327deb882cf99';
->>>>>>> ffe6698cd546cf8344ac4f556b98cb2124297649
